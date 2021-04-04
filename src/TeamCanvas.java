136d0104ac99195ddc3dd0c112fc0f761ec0fd8f
Noam <gal.n@husky.neu.edu>
2021-04-04 14:06:33 -0400

    deleting overrides

--- src/TeamCanvas.java
+++ src/TeamCanvas.java
@@ -1,17 +1,4 @@
 public class TeamCanvas extends AbstractCanvas {
-  @Override
-  public void xButton() {
 
-  }
-
-  @Override
-  public void goBackButton() {
-
-  }
-
-  @Override
-  public void logOutButton() {
-
-  }
 }
 
