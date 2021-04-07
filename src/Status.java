88ea23e2c25fb1a33928140df823881b79f0c7bd
cwil290831 <cwil290831@gmail.com>
2021-04-06 20:28:49 -0400

    added to Status Class

--- src/Status.java
+++ src/Status.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class Status extends JPanel implements ActionListener {
   JButton changeStatus;
   JPanel statusPanel;
+  ActionListener listener;
   // Create button panel for change status button
   // Currently: creating a status panel to handle changes, current status
 
@@ -17,10 +18,34 @@ public class Status extends JPanel implements ActionListener {
     changeStatus.setHorizontalTextPosition(AbstractButton.CENTER);
     changeStatus.setMnemonic(KeyEvent.VK_M);
     statusPanel.add(changeStatus);
+
+    changeStatus.addActionListener(this.listener);
   }
 
   //Placeholder
   public void actionPerformed(ActionEvent e) {
+       JPanel statusChange = new JPanel(false);
+       JButton available = new JButton("Available");
+       JButton busy = new JButton("Busy");
+       JButton away = new JButton("Away");
+       JButton outOfOffice = new JButton("Out of Office");
+
+    available.setVerticalTextPosition(AbstractButton.BOTTOM);
+    available.setHorizontalTextPosition(AbstractButton.CENTER);
+    statusPanel.add(available);
+
+    busy.setVerticalTextPosition(AbstractButton.BOTTOM);
+    busy.setHorizontalTextPosition(AbstractButton.CENTER);
+    statusPanel.add(busy);
+
+    away.setVerticalTextPosition(AbstractButton.BOTTOM);
+    away.setHorizontalTextPosition(AbstractButton.CENTER);
+    statusPanel.add(away);
+
+    outOfOffice.setVerticalTextPosition(AbstractButton.BOTTOM);
+    outOfOffice.setHorizontalTextPosition(AbstractButton.CENTER);
+    statusPanel.add(outOfOffice);
+
 
   }
 
